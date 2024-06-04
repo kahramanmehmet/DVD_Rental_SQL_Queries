bc2327d93d8ea2fdb11f06011788c70a08e47f0d
Mehmet Kahraman <123094567+kahramanmehmet@users.noreply.github.com>
2024-06-04 11:42:54 +0300

    Update Homework_7.sql

--- Homework_7.sql
+++ Homework_7.sql
@@ -6,15 +6,29 @@
 -- Q4: city tablosunda bulunan şehir verilerini country_id sütununa göre gruplandırdıktan sonra en fazla şehir sayısı barındıran country_id bilgisini ve şehir sayısını paylaşınız.
 
 -- A1:
-SELECT rating, COUNT(*) FROM film GROUP BY rating;
+SELECT rating, COUNT(*)
+FROM film
+GROUP BY rating;
 
 -- A2:
-SELECT replacement_cost, COUNT(*) FROM film GROUP BY replacement_cost HAVING COUNT(*) > 50 ORDER BY replacement_cost ASC;
+SELECT replacement_cost,
+COUNT(*)
+FROM film
+GROUP BY replacement_cost
+HAVING COUNT(*) > 50
+ORDER BY replacement_cost ASC;
 
 -- A3:
-SELECT store_id, COUNT(*) FROM customer GROUP BY store_id;
+SELECT store_id,
+COUNT(*)
+FROM customer
+GROUP BY store_id;
 
 -- A4:
-SELECT country_id, COUNT(*) FROM city GROUP BY country_id ORDER BY COUNT(*) DESC;
+SELECT country_id,
+COUNT(*)
+FROM city
+GROUP BY country_id
+ORDER BY COUNT(*) DESC;
 
 -- (country_id = 44, count = 60)
