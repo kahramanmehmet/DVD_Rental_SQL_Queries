b22a052791e1ba6c862b78099742b135de41abf5
Mehmet Kahraman <123094567+kahramanmehmet@users.noreply.github.com>
2024-06-04 11:45:55 +0300

    Update Homework_5.sql

--- Homework_5.sql
+++ Homework_5.sql
@@ -5,10 +5,19 @@
 -- Q3: customer tablosunda bulunan last_name sütununa göre azalan yapılan sıralamada store_id 1 olmak koşuluyla ilk 4 veriyi sıralayınız.
 
 -- A1:
-SELECT * FROM film WHERE  title LIKE '%n' ORDER BY length DESC LIMIT 5;
+SELECT * FROM film 
+WHERE  title LIKE '%n'
+ORDER BY length DESC
+LIMIT 5;
 
 -- A2:
-SELECT * FROM film WHERE title LIKE '%n' ORDER BY length ASC OFFSET 5 LIMIT 5;
+SELECT * FROM film
+WHERE title LIKE '%n'
+ORDER BY length ASC OFFSET 5
+LIMIT 5;
 
 -- A3:
-SELECT * FROM customer WHERE store_id = 1 ORDER BY last_name DESC LIMIT 4;
+SELECT * FROM customer
+WHERE store_id = 1
+ORDER BY last_name DESC
+LIMIT 4;
